f0bac1218c0f990ef3fdd05253f5e06420e730fe
Mark Lanett <mark.lanett@gmail.com>
2012-01-21 19:19:56 -0800

    Simple test - but doesn't actually work.

--- lib/redis-lock.rb
+++ lib/redis-lock.rb
@@ -4,6 +4,45 @@ require "redis-lock/version"
 class Redis
 
   class Lock
+    # @param redis is a Redis instance
+    # @param key is a unique string identifying the object to lock, e.g. "user-1"
+    def initialize( redis, key )
+      @redis  = redis
+      @key    = key
+    end
+
+    def lock( timeout = 1, &block )
+      acquire_lock(timeout) or raise LockNotAcquired.new(key)
+      if block then
+        begin
+          block.call
+        ensure
+          release_lock
+        end
+      end
+    end
+
+    def unlock
+      release_lock
+    end
+
+    def acquire_lock( timeout )
+        true
+    end
+
+    def release_lock
+      true
+    end
   end # Lock
+
+  # Convenience methods
+
+  def lock( key, timeout = 1, &block )
+    Lock.new( self, key ).lock( timeout, &block )
   end
-end
+
+  def unlock( key )
+    Lock( self, key ).unlock
+  end
+
+end # Redis
--- spec/redis_lock_spec.rb
+++ spec/redis_lock_spec.rb
@@ -3,5 +3,10 @@ require "helper"
 describe Redis::Lock, redis: true do
 
   it "can acquire and release a lock"
+  it "can acquire and release a lock" do
+    redis.lock("test") { true }
+    expect { redis.lock("test") { true } }.to_not raise_exception
+    expect { redis.lock("test") { true } }.to_not raise_exception
+  end
 
 end
