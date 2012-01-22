f39c49715848c3a17bfc50c8af4de734ebd36d08
Mark Lanett <mark.lanett@gmail.com>
2012-01-21 20:41:44 -0800

    Implement locking.

--- lib/redis-lock.rb
+++ lib/redis-lock.rb
@@ -8,14 +8,25 @@ class Redis
     class LockNotAcquired < StandardError
     end
 
+    attr :okey        # key with redis namespace
+    attr :oval
+    attr :xkey        # expiration key with redis namespace
+    attr :xval
     attr :life        # how long we expect to keep this lock locked
+    attr :locked
+
     # @param redis is a Redis instance
     # @param key is a unique string identifying the object to lock, e.g. "user-1"
-    def initialize( redis, key )
     # @param options[:life] may be set, but defaults to 1 minute
+    # @param options[:owner] may be set, but defaults to HOSTNAME:PID
+    def initialize( redis, key, options = {} )
       @redis  = redis
       @key    = key
+      @okey   = "lock:owner:#{key}"
+      @oval   = options[:owner] || "#{`hostname`.strip}:#{Process.pid}"
+      @xkey   = "lock:expire:#{key}"
       @life   = options[:life] || 60
+      @locked = false
     end
 
     def lock( timeout = 1, &block )
@@ -34,12 +45,44 @@ class Redis
     end
 
     def acquire_lock( timeout )
-      with_timeout(timeout) do
-        true
+      @locked = false
+      with_timeout(timeout) { successfully_locked_key? }
+      @locked
+    end
+
+    # @returns true if locked, false otherwise
+    def successfully_locked_key?( tries = 2 )
+
+      # We need to set both owner and expire at the same time
+      # If the existing lock is stale, we try again once
+
+      loop do
+        try_xval = Time.now.to_i + life
+        result   = redis.msetnx okey, oval, xkey, try_xval
+
+        if result == 1 then
+          log "successfully_locked_key?() success"
+          @xval   = try_xval
+          @locked = true
+          return true
+
+        else
+          log "successfully_locked_key?() failed"
+          # consider the possibility that this lock is stale
+          tries -= 1
+          next if tries > 0 && stale_key?
+          return false
+        end
       end
     end
 
+    def stale_key?
+      false
+    end
+
     def release_lock
+      redis.del okey
+      redis.del xkey
       true
     end
 
