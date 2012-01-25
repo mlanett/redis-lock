74488887531968cbac99e3dbb5f23bbd9ba40760
Mark Lanett <mark.lanett@gmail.com>
2012-01-25 07:59:24 -0700

    minor redo of specs

--- spec/redis_lock_spec.rb
+++ spec/redis_lock_spec.rb
@@ -2,6 +2,14 @@ require "helper"
 
 describe Redis::Lock, redis: true do
 
+  before do
+    @a_owner = "first"
+    @b_owner = "second"
+    @a  = Redis::Lock.new( redis, "one", owner: @a_owner )
+    @b1 = Redis::Lock.new( redis, "one", owner: @b_owner )
+    @b2 = Redis::Lock.new( redis, "two", owner: @b_owner )
+  end
+
   it "can acquire and release a lock" do
     redis.lock("key") { true }
     expect { redis.lock("key") { true } }.to_not raise_exception
@@ -10,29 +18,28 @@ describe Redis::Lock, redis: true do
 
 
   it "can acquire a lock" do
-    a = Redis::Lock.new( redis, "key" )
-    a.do_lock.should be_true
+    @a.do_lock.should be_true
   end
 
-  it "can release a lock"
-  # should test release_lock
+  it "can release a lock" do
+    @a.lock
+    @a.release_lock
+  end
 
   it "can use a timeout" do
-    it = Redis::Lock.new( redis, "key" )
-    it.with_timeout(1) { true }.should be_true
-    it.with_timeout(1) { false }.should be_false
+    @a.with_timeout(1) { true }.should be_true
+    @a.with_timeout(1) { false }.should be_false
     # a few attempts are OK
     results = [ false, false, true ]
-    it.with_timeout(1) { results.shift }.should be_true
+    @a.with_timeout(1) { results.shift }.should be_true
     # this is too many attemps
     results = [ false, false, false, false, false, true ]
-    it.with_timeout(1) { results.shift }.should be_false
+    @a.with_timeout(1) { results.shift }.should be_false
   end
 
   it "does not take too long to time out" do
-    it = Redis::Lock.new( redis, "key" )
     start = Time.now.to_f
-    it.with_timeout(1) { false }
+    @a.with_timeout(1) { false }
     time = Time.now.to_f - start
     time.should be_within(0.2).of(1.0)
   end
